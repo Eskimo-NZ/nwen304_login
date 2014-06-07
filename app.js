4f49b6331700cefce85a039b426e8217b9556c26
Eskimo-NZ <alex.candler@gmail.com>
2014-06-07 23:23:43 +1200

    Removed other login types

--- app.js
+++ app.js
@@ -10,14 +10,9 @@ var http = require('http');
 var path = require('path');
 var passport = require('passport');
 var FacebookStrategy = require('passport-facebook').Strategy;
-var GitHubStrategy = require('passport-github').Strategy;
-
 
 var FACEBOOK_APP_ID = "566740760110796"
 var FACEBOOK_APP_SECRET = "ae1c43ba534dfdfe394c6e66e08dfcce";
-
-var GITHUB_CLIENT_ID = "your client id"
-var GITHUB_CLIENT_SECRET = "your client secret";
 
 var app = express();
 
@@ -77,32 +72,9 @@ app.get('/error', function(req, res){
 	res.send("error logged in");
 });
 
-
 app.get('/', function(req, res){
   res.sendfile('./views/auth.html');
 });
-
-
-
-
-passport.use(new GitHubStrategy({
-        clientID: GITHUB_CLIENT_ID,
-        clientSecret: GITHUB_CLIENT_SECRET,
-  	callbackURL: "http://localhost:3000/auth/github/callback"
-    },
-    function (accessToken, refreshToken, profile, done) {
-        process.nextTick(function () {
- 	    console.log(profile);
-            return done(null, profile);
-        });
-    }
-));
-
-app.get('/auth/github', passport.authenticate('github'));
-
-app.get('/auth/github/callback', 
-  passport.authenticate('github', {successRedirect: '/success',
-  						failureRedirect:'/error'}));
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
