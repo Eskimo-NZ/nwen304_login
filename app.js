31df07243fb6147ccf54c1fdf5181ca1d3b2e17b
Eskimo-NZ <alex.candler@gmail.com>
2014-06-10 20:02:32 +1200

    Added database code to app.js

--- app.js
+++ app.js
@@ -17,9 +17,9 @@ var FACEBOOK_APP_SECRET = "ae1c43ba534dfdfe394c6e66e08dfcce";
 var app = express();
 
 // Database
-var pg = require('pg').native
-var connectionString = process.env.DATABASE_URL
-var port = process.env.PORT
+var pg = require('pg').native;
+var connectionString = process.env.DATABASE_URL;
+var port = process.env.PORT;
 var client = new pg.Client(connectionString);
 
 client.connect();
