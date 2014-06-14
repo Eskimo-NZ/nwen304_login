35f60538b682c0886c4d983b624946caeb87e352
Eskimo-NZ <alex.candler@gmail.com>
2014-06-15 09:09:52 +1200

    Added user id to success page

--- app.js
+++ app.js
@@ -34,7 +34,6 @@ app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
-app.use(express.session({ secret: 'keyboard cat' }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(app.router);
